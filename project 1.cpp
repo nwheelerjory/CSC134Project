8415520f56f8843528389dd2fb6b6c3346dbe55e
nwheelerjory <43829243+nwheelerjory@users.noreply.github.com>
2021-03-02 08:10:38 -0500

    Input
    Added code for getting user input.

---
+++ project 1.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	double gpuclkspeed = 0, cpuclkspeed = 0, cores = 0;
+	
+	cout << "Enter GPU clock speed in Megahertz: ";
+	cin >> gpuclkspeed;
+	
+	cout << "Enter CPU clock speed in Megahertz: ";
+	cin >> cpuclkspeed;
+	
+	cout << "Enter number of processor cores: ";
+	cin >> cores;
+	
+	cout << "\nSelect monitor resolution:\n";
+	cout << "1. 1280 x 720\n";
+	cout << "2. 1920 x 1080\n";
+	cout << "3. 2560 x 1440\n";
+	cout << "4. 3840 x 2160\n";
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
