a15a008ccbd2021dcacfa7c59d9bd34aed45bf79
nwheelerjory <43829243+nwheelerjory@users.noreply.github.com>
2021-03-02 08:33:45 -0500

    Performance Score
    Added code for determining performance score and recommended graphics quality.

--- project 1.cpp
+++ project 1.cpp
@@ -3,7 +3,8 @@ using namespace std;
 
 int main() {
 	
-	double gpuclkspeed = 0, cpuclkspeed = 0, cores = 0;
+	double gpuclkspeed = 0, cpuclkspeed = 0, cores = 0, perfscore = 0;
+	int res = 0;
 	
 	cout << "Enter GPU clock speed in Megahertz: ";
 	cin >> gpuclkspeed;
@@ -19,6 +20,78 @@ int main() {
 	cout << "2. 1920 x 1080\n";
 	cout << "3. 2560 x 1440\n";
 	cout << "4. 3840 x 2160\n";
+	cin >> res;
+	
+	if (res == 1) {
+		
+		perfscore = ((5.0 * gpuclkspeed) + (cores * cpuclkspeed)) * 1;
+		
+	}
+	
+	else if (res == 2) {
+		
+		perfscore = ((5.0 * gpuclkspeed) + (cores * cpuclkspeed)) * .75;
+		
+	}
+	
+	else if (res == 3) {
+		
+		perfscore = ((5.0 * gpuclkspeed) + (cores * cpuclkspeed)) * .55;
+		
+	}
+	
+	else if (res == 4) {
+		
+		perfscore = ((5.0 * gpuclkspeed) + (cores * cpuclkspeed)) * .35;
+		
+	}
+	
+	else{
+		
+		cout << "Error\n";
+		
+	}
+	
+	cout << "\nRecommended Graphics Quality:\n";
+	
+	if (perfscore <= 11000) {
+		
+		cout << "Unplayable\n";
+		
+	}
+	
+	else if (perfscore > 11000 && perfscore <= 13000) {
+		
+		cout << "Low\n";
+		
+	}
+	
+	else if (perfscore > 13000 && perfscore <= 15000) {
+		
+		cout << "Medium\n";
+		
+	}
+	
+	else if (perfscore > 15000 && perfscore <= 17000) {
+		
+		cout << "High\n";
+		
+	}
+	
+	else if (perfscore > 17000){
+		
+		cout << "Ultra\n";
+		
+	}
+	
+	else{
+		
+		cout << "Error";
+		
+	}
+	
+	
+	
 	
 	
 	
